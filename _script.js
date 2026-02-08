3dd35d9432a5285544c723f3dd5ec0ae3cb12161
mohammadyy666 <Iran1388.888@gmail.com>
2026-02-08 16:24:22 +0330

    Delete _script.js

--- _script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("startBtn").addEventListener("click", () => {
-  document.getElementById("services")
-    .scrollIntoView({behavior:"smooth"});
-});
-
-document.getElementById("contactForm").addEventListener("submit", e => {
-  e.preventDefault();
-  alert("پیام شما ارسال شد ✅");
-});
